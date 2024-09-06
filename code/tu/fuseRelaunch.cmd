542078cf3407beb7bfaeef838b3e4f064f7efbb4
70nKA <grbavacantonijaa@gmail.com>
2024-09-06 18:35:05 +0200

    [tu] UART: 115200 baud rate compensating generator implemented

---
+++ code/tu/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "/home/ise/code/tu/binary_16b_counter_test_isim_beh.exe" -prj "/home/ise/code/tu/binary_16b_counter_test_beh.prj" "binary_16b_counter_test" "work.glbl" 
